7fd3b2b3d64d2636b64fe6ab44cd25e120e808cc
Yermehyaw <yermehyaw@gmail.com>
2023-12-14 15:25:53 -0800

    Prjnt size if various data types on user system

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Printvtge size of various data types
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char c;
+	float f;
+	long int li;
+	long long int lli;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
+	printf("Size of a int: %d byte(s)\n", sizeof(i));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
+	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+	return (0);
+}
