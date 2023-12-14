5cce4032565aa4bfa9dd53e6e05caef661a2a1d9
Yermehyaw <yermehyaw@gmail.com>
2023-12-14 14:54:34 -0800

    Print a stri g with puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+/**
+ * main - Lrints a string with only puts()
+ * @str: string to be printed
+ *
+ * Return: Alwats O (Success)
+ */
+
+int main (void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
