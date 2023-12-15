095ed8db12f65c9d452e421505b1ac3602b14d6e
Yermehyaw <yermehyaw@gmail.com>
2023-12-14 16:07:51 -0800

    Fix checker error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@
 
 int main(void)
 {
-	char *string = "and that piece of art is useful\"
- - Dora Korpar, 2015-10-19\n";
+	char *string = "and that piece of art is useful\""
+" - Dora Korpar, 2015-10-19\n";
 
 	fwrite(string, sizeof(char), strlen(string), stderr);
 	return (1);
