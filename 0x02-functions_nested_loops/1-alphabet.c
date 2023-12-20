ec5354bf92655fce604f6a59a8e7383b9ed67d7e
Yermehyaw <yermehyaw@gmail.com>
2023-12-19 17:28:34 -0800

    Use user function to print a loop of chars

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints a-z
+ *
+ * Return: O (Success)
+ */
+
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+	return;
+}
