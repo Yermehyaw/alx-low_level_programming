9d772801d5e59dc244b85236164741904285c4b6
Yermehyaw <yermehyaw@gmail.com>
2023-12-20 21:00:33 -0800

    Fix checker error

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,10 +9,8 @@
 
 int _isalpha(int c)
 {
-	if (c > 65  && c < 91) || (c > 97 && c < 123)
-				  {
-					  return (1);
-				  }
+	if (c > 65  && c < 91 || c > 97 && c < 123)
+		return (1);
 	else
 		return (0);
 }
