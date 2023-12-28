d98acf4714751b998d4015ec18eac0758b087960
Yermehyaw <yermehyaw@gmail.com>
2023-12-28 14:17:11 -0800

    Fix checker error

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,9 +16,14 @@ void print_diagonal(int n)
 		_putchar('\n');
 	else
 	{
-		for (m = 0; m <= n; ++m)
+
+		for (m = 0; m < n; ++m) /* m < n, because m is init to  0 */
 		{
 			p = m;/* Spaces increases with diagonal length */
+			/* p -= 1; */  /**
+				 * but less than m by one,
+				 * just an unncecessary alx req
+				 */
 			while (p > 0)
 			{
 				_putchar(' ');
