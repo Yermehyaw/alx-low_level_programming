2d928bad583cca4a7570949a89435f8fc0f9944e
Yermehyaw <yermehyaw@gmail.com>
2023-12-28 15:13:03 -0800

    FizzBuzx coding question

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Fizzbuzz coding interview question
+ *
+ * Return: O (Success)
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if ((i % 15) == 0)
+			printf("FizzBuzz ");
+		else if ((i % 3) == 0)
+			printf("Fizz ");
+		else if ((i % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
