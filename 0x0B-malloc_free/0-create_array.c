5c4100436e4865f4c2be050f4f45ccfacf69e008
Yermehyaw <yermehyaw@gmail.com>
2024-01-15 13:34:19 -0800

    Create and initialize a dynamic char array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Create and intitialize a dynamic array
+ * @size: Array size
+ * @c: Initialization character
+ *
+ * Return: Pointer to array (Success)
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *m;
+
+	if (size == 0)
+		return ('\0');
+	m  = malloc(size); /* 1 is the size of one byte */
+	if (*m == '\0')
+		return ('\0');
+	i = 0;
+	while (size--)
+	{
+		m[i] = c;
+		i++;
+	}
+	return (&(m[0]));
+}
