19f77af49879bf6acd259d46f8f84901e2eaf696
Yermehyaw <yermehyaw@gmail.com>
2024-01-15 15:21:29 -0800

    Concatenate two parameter strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdlib.h>
+
+int len_str(char *);
+
+/**
+ * str_concat - Concatenates two strings passed as parameters
+ * @s1: String 1
+ * @s2: String 2
+ *
+ * Return: A pointer to the concatenated string (Success)
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *m;
+	int i, j;
+
+	m = malloc(len_str(s1) + len_str(s2));
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		m[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		m[i] = s2[j];
+		i++;
+		j++;
+	}
+	m[i] = '\0';
+	return (m);
+}
+
+
+/**
+ * len_str - Return the length of a string
+ * @str: String
+ *
+ * Return: Length of the string
+ */
+
+int len_str(char *str)
+{
+	int len;
+
+	len = 0;
+	if (str == NULL)
+		return (len);
+	while (str[len] != '\0')
+		len++;
+	len += 1;
+	return (len);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,5 +4,6 @@
 void _putchar(int);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *, char *);
 
 #endif /* MAIN_H */
