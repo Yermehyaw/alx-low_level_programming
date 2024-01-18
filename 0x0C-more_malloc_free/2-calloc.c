11396f0a824868ce1a63a19be8e2de513581e0cc
Yermehyaw <yermehyaw@gmail.com>
2024-01-18 14:44:06 -0800

    Use malloc() like calloc()

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocate memory using malloc() like calloc()
+ * @nmemb: No of array elements
+ * @size: Memory size for all elements
+ *
+ * Return: Pointer to location of allocated memory (Success)
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *m;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	m = malloc(nmemb * sizeof size);
+	if (m == NULL)
+		return (NULL);
+	i = 0;
+	while (i < nmemb)
+	{
+		m[i] = 0;
+		++i;
+	}
+	return (m);
+}
