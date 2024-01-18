60f1d26773542f1a628c23a0b858fd792d756c76
Yermehyaw <yermehyaw@gmail.com>
2024-01-18 15:57:42 -0800

    Use malloc() like calloc()[hot-fix3]

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	m = malloc(nmemb * size);
+	m = malloc(nmemb);
 	if (m == NULL)
 		return (NULL);
 	i = 0;
