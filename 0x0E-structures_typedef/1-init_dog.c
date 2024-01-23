fe073bf9b23e95a18acf68ff93bda82d150c51af
Yermehyaw <yermehyaw@gmail.com>
2024-01-23 14:07:22 -0800

    Intialize a struct variable[Additions]

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,7 +11,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
+	if (d == NULL || name == NULL || owner == NULL)
 		exit(98);
 	d->name = name;
 	d->age = age;
