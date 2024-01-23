f548e71b78d6ca6129a438d8c8a29da1d1960822
Yermehyaw <yermehyaw@gmail.com>
2024-01-23 13:17:18 -0800

    Create a struct to hold dog info

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Information on a dog
+ * @nane: Name of the dog
+ * @age: Dogs age
+ * @owner: Dogs owner
+ */
+
+struct dog
+{
+	char *nane;
+	float age;
+	char *owner;
+};
+
+#endif /* #ifndef DOG_H */
