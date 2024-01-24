44ec15da81ba0ed032eb7f6523ea048ab006df38
Yermehyaw <yermehyaw@gmail.com>
2024-01-23 16:25:09 -0800

    Free a struct var

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * free_doog - Frees dynalic mem of a strucy var
+ * @d: Struct variable
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
