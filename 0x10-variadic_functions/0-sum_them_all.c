974e242206acaa288222f15a63e37558d1e307f7
Yermehyaw <yermehyaw@gmail.com>
2024-01-25 14:52:22 -0800

    Func: Return the sum of varying input param

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * sum_them_all - Return sum of all its param
+ * @n: No of param/argumets
+ *
+ * Return: Any value (Success), 0 if unsucessful
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum;
+	va_list ap;
+
+	if (n == 0)
+		return (0);
+	va_start (ap, n);
+	i = 0;
+	while (i < n)
+	{
+		sum += va_arg(ap, int);
+		++i;
+	}
+	va_end (ap);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef VARIADIC_FUNCTIOMS_H
+#define VARIADIC_FUNCTIOMS_H
+
+void _putchar(int);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+#endif /* VARIADIC_FUNCTIOMS_H */
