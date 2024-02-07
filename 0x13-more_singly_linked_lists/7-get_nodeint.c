48b54f3c2549313385cb76bbec9d27414a988907
Yermehyaw <yermehyaw@gmail.com>
2024-02-07 12:34:19 -0800

    Return nth node [hot-fix]

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -20,7 +20,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	temp = head;
 	if (head == NULL)
 		return (NULL);
-	++count;
+	/*++count;*/
 	while (count != 0)
 	{
 		if (temp == NULL)
