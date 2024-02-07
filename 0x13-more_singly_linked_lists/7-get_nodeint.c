2bd642c2ff6aeca6bed0d731479e8ceb8a4f70a5
Yermehyaw <yermehyaw@gmail.com>
2024-02-07 02:38:13 -0800

    Return the address of the nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,7 +19,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	++count;
 	while (count != 0)
 	{
-		if (head->next == NULL)
+		if (head == NULL)
 			return (NULL);
 		head = head->next;
 		--count;
