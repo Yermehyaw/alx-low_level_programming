89807acce944eaf73255244a2a17eb2451d60319
Yermehyaw <yermehyaw@gmail.com>
2024-02-07 08:39:26 -0800

    Return the nth node of a list[fix-0]

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -17,10 +17,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	count = index;
 	++count;
+	if (head == NULL)
+		return (NULL);
 	while (count != 0)
 	{
 		if (head == NULL)
-			return (NULL);
+			break;
 		head = head->next;
 		--count;
 	}
