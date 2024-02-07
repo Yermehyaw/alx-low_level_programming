0098231ef1044ca0ee7912d95230e035adf566ad
Yermehyaw <yermehyaw@gmail.com>
2024-02-07 08:15:52 -0800

    Free a list[hot-fix]

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,10 +13,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (*head == NULL)
+		return;
 	while (*head != NULL)
 	{
 		temp = (*head)->next;
 		free(*head);
-		*head = temp;
+		*head = temp; /* Sets head to NULL at final iteration */
 	}
 }
