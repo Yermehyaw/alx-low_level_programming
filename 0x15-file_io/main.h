382b43aa56ea113617b2621da6dd81228689ecf2
Yermehyaw <yermehyaw@gmail.com>
2024-02-12 12:44:07 -0800

    Update header file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * ssize_t - typedef for int
+ */
+
+typedef int ssize_t;
+
+
+
+
+/* Function Prototypes */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+
+#endif /* MAIN_H */
