62e0ccaf6bd527a8fb49b6a821479a766cc0d604
Yermehyaw <yermehyaw@gmail.com>
2024-02-12 16:27:48 -0800

    Update header file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - Reads abd prints a file to stdout
+ * @filename: Name of file
+ * @letters: Size of char to be printed
+ *
+ * Return: No of expected char to be printed or 0 otherwise (Success)
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, sz_rd, sz_wr;
+	char *buf;
+
+	if (filename == NULL)
+		exit(0);
+	buf = malloc(letters * sizeof(char));
+	if (buf == NULL)
+		exit(0);
+	/* Open the file */
+	fd = open(filename, O_RDWR);
+	if (fd < 0)
+		exit(0);
+	/* Read the file to buf */
+	sz_rd = read(fd, buf, letters);
+	if (sz_rd < 0)
+		exit(0);
+	/* Write to stdout */
+	sz_wr = write(STDOUT_FILENO, buf, letters); /* Change var letters*/
+	if (sz_wr < 0)
+		exit(0);
+	if (close(fd) < 0)
+		exit (0);
+	free(buf);
+	return (sz_wr);
+}
