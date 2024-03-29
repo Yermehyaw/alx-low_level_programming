b4f9dccdad53c7e491d3e284802360ca005af0a4
Yermehyaw <yermehyaw@gmail.com>
2024-03-29 10:21:13 -0700

    Func: Print all int data in a linked list[fix-1]

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,17 +12,17 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i;
-/*	const dlistint_t *const_temp;*/
 	dlistint_t *temp;
 
 	if (h == NULL)
 		return (0);
-/*	const_temp = h;*/
 	temp = (dlistint_t *)h;
-	for (i = 1; temp != NULL; ++i)
+	i = 0;
+	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
 		temp = temp->next;
+		++i;
 	}
 	return (i);
 }
