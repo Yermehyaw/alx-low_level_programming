6d3b536050401ab9a407144ae3a3270a0db7c253
Yermehyaw <yermehyaw@gmail.com>
2024-04-15 14:19:53 -0700

    Fix some compile err

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,27 +10,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash;
-	hash_node_t *hash_array[200];
+	hash_node_t *hash_array;
 	int i;
 
 	new_hash = malloc(sizeof(hash_table_t));
 	if (new_hash == NULL)
 		return (NULL);
 	new_hash->size = size; /* size is the size of array param entered */
-	new_hash->array = &hash_array[0];
+	hash_array = malloc(size * sizeof(hash_node_t));/*each cell is a  struct*/
+	if (hash_array == NULL)
+		return (NULL);
 	i = 0;
 	while (i < (int)size)
 	{
-		hash_array[i] = malloc(sizeof(hash_node_t)); /**
-							      *each cell is a
-							      * struct
-							      */
-		if (hash_array[i] == NULL)
-			return (NULL);
-		hash_array[i]->key = NULL;
-		hash_array[i]->value = NULL;
-		hash_array[i]->next = NULL;
+		hash_array[i] = NULL;
 		++i;
 	}
+	new_hash->array = &hash_array;
 	return (new_hash); /* the new hash table with malloc'ed cells*/
 }
