b6303a9e7b87f9de49f6ef103732f7a245be9271
Yermehyaw <yermehyaw@gmail.com>
2024-04-16 11:02:27 +0100

    Fix algorithm errs

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t **arr;
-	hash_node_t *cell_node, temp;
+	hash_node_t *cell_node, *temp;
 
 	if (ht == NULL)
 		return (0);
@@ -26,30 +26,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (strcmp(key, "") || strcmp(key, NULL)) /* key cannot be empty */
 		return (0);
 	index = key_index(key, ht->size); /* Generate index */
-	if (arr[index] != NULL) /**
-				 * != NULL preferred incase @ht didnt have its
-				 * elements initialized to NULL
-				 */
-	{
-		temp = arr[index];/*temp now holds the array cell mem address*/
-		cell_node = malloc(sizeof(hash_node_t));
-		if (cell_node == NULL)
-			return (0);
+	cell_node = malloc(sizeof(hash_node_t));
+	if (cell_node == NULL)
+		return (0);
+	temp = arr[index];/*temp now holds the array cell mem address*/
+	if (temp != NULL) /**
+			   * a node already exists in this index.
+			   * != NULL preferred incase @ht didnt have its
+			   * elements initialized to NULL. (Im not quite
+			   * "expecting" you to understand... :))
+			   */
 		/* Add new cell node to beginning of linked list*/
 		cell_node->next = temp->next;/**
-					      *point new cell *next to whatever
-					      *arr[index] was initiall pointing
-					      *to i.e NULL or a mem address
+					      * points cell_node's *next to
+					      * whatever arr[index] was
+					      * initially pointing to i.e either
+					      * NULL or a mem address
 					      */
-		arr[index] = cell_node;
-		strcpy(cell_node->value = value);/* initialize irs value & key*/
-		strcpy(arr[index]->key, key)
-		return (1);
-	}
-	else
-	{
-		strcpy(arr[index]->value, value);
-		strcpy(arr[ibdex]->key, key);
-	}
+	arr[index] = cell_node; /*mem addr at that index is now cell_node's*/
+	strcpy(cell_node->value, value);/* initialize irs value & key*/
+	strcpy(cell_node->key, key);
 	return (1);
 }
