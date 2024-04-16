d356b8b504b71e534f6d58d4066a23695efb668f
Yermehyaw <yermehyaw@gmail.com>
2024-04-16 01:10:24 +0100

    Return index of a hashed key/string

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Returns an index of a hash table corresponding to a  key(string)
+ * @key: String to be used in generating a hashed value
+ * @size: size of tge array
+ *
+ * Return: index of the array of a hash table
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash_value;
+
+	hash_value = djb2(key);
+	return (hash_valie % size);
+}
