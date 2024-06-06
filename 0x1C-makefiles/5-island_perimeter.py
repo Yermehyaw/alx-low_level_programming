fb82dad69e971234c298f8d0437c7f666ef77ef1
Yermehyaw <yermehyaw@gmail,com>
2024-06-06 10:23:34 +0100

    Island perimeter interview question

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#/usr/bin/python3
+
+"""
+Modules Imported: None
+"""
+
+
+def island_perimeter(self, grid):
+    """
+    Returns the perimeter of grid
+
+    Args:
+    grid (list): list of list of integers
+
+    Return:
+    Perimeter of grid
+
+    Pseudo:
+
+
+    """
+    
